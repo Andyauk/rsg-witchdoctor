0cbfcec9a2938c4c27c3ce5760c53e3547ee3267
Andyauk <47583709+Andyauk@users.noreply.github.com>
2023-12-28 11:05:50 +0000

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -11,8 +11,7 @@ end
 
 local function CheckVersion()
     PerformHttpRequest(
-        --'https://raw.githubusercontent.com/Rexshack-RedM/rsg-witchdoctor/main/version.txt',
-        'https://raw.githubusercontent.com/Andyauk/rsg-witchdoctor/main/version.txt', --temp update in
+        'https://raw.githubusercontent.com/Andyauk/rsg-witchdoctor/main/version.txt',
         function(err, text, headers)
             local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
 
